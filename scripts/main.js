608421cc43fb3153186f96d0fdec54b5b63ebcc0
Brett Vonbbr <vonbbr@yahoo.com>
2018-07-18 10:44:45 -0500

    website add

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Firefox-Logo.png') {
+        myImage.setAttribute ('src', 'images/firefox-logo-2.png');
+    } else {
+        myImage.setAttribute ('src', 'images/Firefox-Logo.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
